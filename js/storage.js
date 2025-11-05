3615b0272e30c1527feba285d0896b3c7e190184
Sushiljat01010101 <mc7705191@gmail.com>
2025-11-05 19:52:53 +0530

    Add files via upload

--- js/storage.js
+++ js/storage.js
@@ -8,8 +8,8 @@ class StorageManager {
     initializeDefaultUser() {
         if (!localStorage.getItem('libraryUser')) {
             const defaultUser = {
-                username: 'aaaa',
-                password: 'aaa',
+                username: 'admin',
+                password: 'admin123',
                 name: 'Administrator'
             };
             localStorage.setItem('libraryUser', JSON.stringify(defaultUser));
@@ -90,6 +90,8 @@ class StorageManager {
             activities: this.getActivities(),
             seats: this.getSeats(),
             settings: this.getSettings(),
+            user: this.getUser(),
+            preferences: this.getPreferences(),
             exportDate: new Date().toISOString(),
             backupType: isMissedBackup ? 'auto-missed' : 'auto'
         };
